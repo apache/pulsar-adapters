13a87c843109473bcb6fa20eefb6d833154060a4
Andrey Yegorov <andrey.yegorov@datastax.com>
2021-04-07 16:21:12 -0700

    Added example

---
+++ examples/kafka-streams/src/main/java/org/apache/kafka/kafkastreams/pulsar/example/LineSplit.java
@@ -0,0 +1,109 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.kafka.kafkastreams.pulsar.example;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * In this example, we implement a simple LineSplit program using the high-level Streams DSL
+ * that reads from a source topic "streams-plaintext-input", where the values of messages represent lines of text;
+ * the code split each text line in string into words and then write back into a sink topic "streams-linesplit-output" where
+ * each record represents a single word.
+ */
+public class LineSplit {
+
+    /*
+    to run:
+    mvn clean package
+    mvn exec:java -Dexec.mainClass=org.apache.kafka.kafkastreams.pulsar.example.LineSplit
+
+    it need running pulsar (standalone)
+
+    topic to read from:
+    bin/pulsar-client consume streams-linesplit-output -s test -p Earliest -n 0
+
+    topic to produce to:
+    bin/pulsar-client produce streams-plaintext-input --messages "I sent a few words"
+    */
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-linesplit");
+
+        // kafka would do something like
+        // props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put("bootstrap.servers", "pulsar://localhost:6650");
+
+        props.put("key.serializer", StringSerializer.class.getName());
+        props.put("value.serializer", StringSerializer.class.getName());
+        props.put("key.deserializer", StringSerializer.class.getName());
+        props.put("value.deserializer", StringDeserializer.class.getName());
+
+        props.put("group.id", "linesplit-subscription-name");
+        props.put("enable.auto.commit", "true");
+
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        /*
+        // kafka would do something like:
+        builder.<String, String>stream("streams-plaintext-input")
+                .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .to("streams-linesplit-output");
+        */
+
+        // note topic names are in the pulsar notation
+        builder.<String, String>stream("persistent://public/default/streams-plaintext-input")
+            .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+            .to("persistent://public/default/streams-linesplit-output");
+
+        final Topology topology = builder.build();
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (Throwable e) {
+            System.exit(1);
+        }
+        System.exit(0);
+    }
+}
---
+++ examples/kafka-streams/src/main/java/org/apache/kafka/kafkastreams/pulsar/example/README.md
@@ -0,0 +1,86 @@
+<!--
+
+    Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+
+-->
+
+## Apache Kafka Streams for Pulsar
+
+This page describes how to use the [Kafka Streams](https://kafka.apache.org/27/documentation/streams/) with Pulsar topics.
+
+## Example
+
+### LineSplit
+
+This Kafka Streams job is consuming from a Pulsar topic, splitting lines into words in a streaming fashion. 
+The job writes the words to another Pulsar topic.
+
+The steps to run the example:
+
+1. Start Pulsar Standalone.
+
+   You can follow the [instructions](https://pulsar.apache.org/docs/en/standalone/) to start a Pulsar standalone locally.
+
+    ```shell
+    $ bin/pulsar standalone
+    ```
+
+2. Create topics to use later. The easiest way to do so is to produce a message to the topic:
+
+   ```shell
+   $ bin/pulsar-client produce streams-plaintext-input --messages "tada"
+   $ bin/pulsar-client produce streams-linesplit-output --messages "tada"
+   ```
+
+3. Build the examples.
+
+    ```shell
+    $ cd ${PULSAR_ADAPTORS_HOME}/examples/kafka-streams
+    $ mvn clean package
+    ```
+
+4. Run the example.
+
+    ```shell
+    $ mvn exec:java -Dexec.mainClass=org.apache.kafka.kafkastreams.pulsar.example.LineSplit
+    ```
+
+5. Produce messages to topic `streams-plaintext-input`.
+
+    ```shell
+    $ bin/pulsar-client produce streams-plaintext-input --messages "I produced a few words"
+    ```
+
+6. You can check that consumer sees the words in output topic `streams-linesplit-output`, e.g.:
+
+    ```shell
+    $ bin/pulsar-client consume streams-linesplit-output -s test -p Earliest -n 0  
+    ```
+   outputs something like
+   ```text
+   ----- got message -----
+   key:[null], properties:[pulsar.partition.id=0], content:I
+   ----- got message -----
+   key:[null], properties:[pulsar.partition.id=0], content:produced
+   ----- got message -----
+   key:[null], properties:[pulsar.partition.id=0], content:a
+   ----- got message -----
+   key:[null], properties:[pulsar.partition.id=0], content:few
+   ----- got message -----
+   key:[null], properties:[pulsar.partition.id=0], content:words
+   ```
