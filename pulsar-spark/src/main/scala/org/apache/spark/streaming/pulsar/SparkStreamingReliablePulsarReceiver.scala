f8917f660432a5c991a8a356e245c405470b6f2b
Aditya Tiwari <aditya.tiwari@flipkart.com>
2021-12-29 15:41:27 +0530

    Adding receiver thread close check and repackaging

--- pulsar-spark/src/main/scala/org/apache/spark/streaming/pulsar/SparkStreamingReliablePulsarReceiver.scala
+++ pulsar-spark/src/main/scala/org/apache/spark/streaming/pulsar/SparkStreamingReliablePulsarReceiver.scala
@@ -1,4 +1,22 @@
-package org.apache.pulsar.spark
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.spark.streaming.pulsar
 
 import com.google.common.util.concurrent.RateLimiter
 import org.apache.pulsar.client.api._
@@ -61,6 +79,7 @@ class SparkStreamingReliablePulsarReceiver(override val storageLevel: StorageLev
 
   private var pulsarClient: PulsarClient = null
   private var consumer: Consumer[Array[Byte]] = null
+  private var consumerThread: Thread = null
 
   var latestStorePushTime: Long = System.currentTimeMillis()
 
@@ -79,15 +98,19 @@ class SparkStreamingReliablePulsarReceiver(override val storageLevel: StorageLev
         restart("Restart a consumer")
     }
     latestStorePushTime = System.currentTimeMillis()
-    new Thread() {
+
+    consumerThread = new Thread() {
       override def run() {
         receive()
       }
-    }.start()
+    }
+
+    consumerThread.start()
 
   }
 
   override def onStop(): Unit = try {
+    if (consumerThread != null && consumerThread.isAlive) consumerThread.stop()  // Ideally consumrThread should be closed beforee calling onStop.
     if (consumer != null) consumer.close()
     if (pulsarClient != null) pulsarClient.close()
   } catch {
