93198bb4be0c0619ff00291228ec3c1bac44b902
swamymavuri <mavuriswamy@gmail.com>
2023-03-27 02:36:10 +0530

    Added test cases with Mockito and testng with assertions

--- pulsar-client-kafka-compat/pulsar-client-kafka/src/test/java/org/apache/kafka/clients/consumer/PulsarKafkaConsumerTest.java
+++ pulsar-client-kafka-compat/pulsar-client-kafka/src/test/java/org/apache/kafka/clients/consumer/PulsarKafkaConsumerTest.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,14 +18,8 @@
  */
 package org.apache.kafka.clients.consumer;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -43,62 +37,47 @@ import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.client.impl.MessageImpl;
 import org.apache.pulsar.client.impl.PulsarClientImpl;
-import org.apache.pulsar.client.util.MessageIdUtils;
 import org.apache.pulsar.common.api.proto.MessageMetadata;
 import org.apache.pulsar.common.naming.TopicName;
-import org.junit.Assert;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PowerMockIgnore;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.testng.IObjectFactory;
-import org.testng.annotations.ObjectFactory;
+import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-@PrepareForTest({TopicName.class, MessageIdUtils.class})
-@PowerMockIgnore({"org.apache.logging.log4j.*"})
 public class PulsarKafkaConsumerTest {
 
-    @ObjectFactory
-    public IObjectFactory getObjectFactory() {
-        return new org.powermock.modules.testng.PowerMockObjectFactory();
-    }
-
     @Test
-    public void testPulsarKafkaConsumerWithHeaders() throws Exception {
-        PowerMockito.mockStatic(TopicName.class);
-        PowerMockito.mockStatic(MessageIdUtils.class);
+    public void testPulsarKafkaConsumerWithHeaders_noAck() throws Exception {
+        TopicName topicName = Mockito.mock(TopicName.class);
+        Hex hex = Mockito.mock(Hex.class);
 
-        TopicName topicName = mock(TopicName.class);
+        Mockito.when(topicName.getPartitionedTopicName()).thenReturn("topic");
 
-        doReturn("topic").when(topicName).getPartitionedTopicName();
-
-        ClientBuilder mockClientBuilder = mock(ClientBuilder.class);
-        Consumer consumer = mock(Consumer.class);
-        MessageId msgId = mock(MessageId.class);
+        ClientBuilder mockClientBuilder = Mockito.mock(ClientBuilder.class);
+        Consumer consumer = Mockito.mock(Consumer.class);
+        Mockito.doReturn("topic").when(consumer).getTopic();
+        MessageId msgId = Mockito.mock(MessageId.class);
         MessageMetadata messageMetadata = new MessageMetadata();
         messageMetadata.setPublishTime(System.currentTimeMillis());
+        String topic = "topic";
+
+        TopicName topicNameAll = TopicName.get(topic);
 
         Map<String, String> headerMap = new HashMap<>();
         String header1 = MessageConstants.KAFKA_MESSAGE_HEADER_PREFIX + "header1";
         String kafkaHeader = MessageConstants.KAFKA_MESSAGE_HEADER_PREFIX + header1;
-        headerMap.put(kafkaHeader,
-                Hex.encodeHexString(header1.getBytes()));
+        headerMap.put(kafkaHeader, Hex.encodeHexString(header1.getBytes()));
         Message<byte[]> msg =
-                new MessageImpl<byte[]>("topic", "1:1", headerMap, "string".getBytes(), Schema.BYTES,
-                        messageMetadata);
+                new MessageImpl<byte[]>(topic, "1:1", headerMap, "string".getBytes(), Schema.BYTES, messageMetadata);
 
+        PulsarClient mockClient = Mockito.mock(PulsarClient.class);
+        PulsarClientImpl mockClientImpl = Mockito.mock(PulsarClientImpl.class);
 
-        PulsarClient mockClient = mock(PulsarClient.class);
-        PulsarClientImpl mockClientImpl = mock(PulsarClientImpl.class);
+        CompletableFuture<Integer> mockNoOfPartitionFuture = CompletableFuture.completedFuture(1);
 
-        CompletableFuture<Integer> mockNoOfPartitionFuture = new CompletableFuture<Integer>();
-        mockNoOfPartitionFuture.complete(1);
+        Mockito.doReturn(mockClientBuilder).when(mockClientBuilder).serviceUrl(Mockito.anyString());
+        Mockito.doReturn(mockClient).when(mockClientBuilder).build();
 
-        doReturn(mockClientBuilder).when(mockClientBuilder).serviceUrl(anyString());
-        when(TopicName.get(any())).thenReturn(topicName);
-        doReturn(mockClient).when(mockClientBuilder).build();
-
-        when(mockClientImpl.getNumberOfPartitions(anyString())).thenReturn(mockNoOfPartitionFuture);
+        Mockito.when(mockClientImpl.getNumberOfPartitions(Mockito.anyString())).thenReturn(mockNoOfPartitionFuture);
 
         Properties properties = new Properties();
 
@@ -107,68 +86,22 @@ public class PulsarKafkaConsumerTest {
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Collections.singletonList("pulsar://localhost:6650"));
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-subscription-name");
 
-
-        PulsarKafkaConsumer<Integer, String> pulsarKafkaConsumerSpy =
-                spy(new PulsarKafkaConsumer<>(properties, new IntegerDeserializer(), new StringDeserializer()));
-
-        doNothing().when(pulsarKafkaConsumerSpy).seekToEnd(anyCollection());
-        PowerMockito.whenNew(PulsarKafkaConsumer.class).withAnyArguments().thenReturn(pulsarKafkaConsumerSpy);
-
         PulsarKafkaConsumer<Integer, String> pulsarKafkaConsumer =
                 new PulsarKafkaConsumer<>(properties, new IntegerDeserializer(), new StringDeserializer());
 
-        pulsarKafkaConsumer.received(consumer, msg);
-        pulsarKafkaConsumer.poll(100);
-        pulsarKafkaConsumer.close();
+        PulsarKafkaConsumer<Integer, String> pulsarKafkaConsumerSpy = Mockito.spy(pulsarKafkaConsumer);
 
+        Mockito.doNothing().when(pulsarKafkaConsumerSpy).seekToEnd(anyCollection());
+
+        pulsarKafkaConsumerSpy.received(consumer, msg);
+        pulsarKafkaConsumerSpy.poll(100);
+        pulsarKafkaConsumerSpy.close();
 
         Assert.assertNotNull(msg.getProperty(kafkaHeader));
-
+        Mockito.verify(pulsarKafkaConsumerSpy).seekToEnd(anyCollection());
+        Mockito.verify(consumer, Mockito.times(0)).acknowledgeCumulativeAsync(Mockito.any(MessageId.class));
+        Mockito.verify(hex, Mockito.times(1)).decodeHex(Hex.encodeHexString(header1.getBytes()));
     }
 
-    @Test
-    public void testPulsarKafkaConsumer() throws Exception {
-        PowerMockito.mockStatic(TopicName.class);
-        PowerMockito.mockStatic(MessageIdUtils.class);
-
-        TopicName topicName = mock(TopicName.class);
-
-        doReturn("topic").when(topicName).getPartitionedTopicName();
-
-        ClientBuilder mockClientBuilder = mock(ClientBuilder.class);
-        Consumer<byte[]> consumer = mock(Consumer.class);
-        Message<byte[]> msg = mock(Message.class);
-        MessageId msgId = mock(MessageId.class);
-
-        PulsarClient mockClient = mock(PulsarClient.class);
-
-        doReturn(mockClientBuilder).when(mockClientBuilder).serviceUrl(anyString());
-        when(TopicName.get(any())).thenReturn(topicName);
-        when(msg.getMessageId()).thenReturn(msgId);
-        doReturn(mockClient).when(mockClientBuilder).build();
-
-        Properties properties = new Properties();
-
-        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class.getName());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, Collections.singletonList("pulsar://localhost:6650"));
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-subscription-name");
-
-
-        PulsarKafkaConsumer<Integer, String> pulsarKafkaConsumerSpy =
-                spy(new PulsarKafkaConsumer<>(properties, new IntegerDeserializer(), new StringDeserializer()));
-
-        doNothing().when(pulsarKafkaConsumerSpy).seekToEnd(anyCollection());
-        PowerMockito.whenNew(PulsarKafkaConsumer.class).withAnyArguments().thenReturn(pulsarKafkaConsumerSpy);
-
-        PulsarKafkaConsumer<Integer, String> pulsarKafkaConsumer =
-                new PulsarKafkaConsumer<>(properties, new IntegerDeserializer(), new StringDeserializer());
-
-        pulsarKafkaConsumer.poll(100);
-        pulsarKafkaConsumer.close();
-    }
 }
-
-
-
 
